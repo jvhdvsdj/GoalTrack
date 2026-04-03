1cdf5d685ed24d59e522d21548eca293717c87f3
jvhdvsdj <romkavasyliuk@gmail.com>
2026-04-03 21:03:16 +0300

    Add task_cards view with pagination for tasks

--- django_project/Tasks/views.py
+++ django_project/Tasks/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from tasks.models import Task
 
-def m_page(request):
-    return render(request, 'base.html')
+def task_cards(request):
+    tasks_1 = Task.objects.all()
+
+    paginator = Paginator(tasks_1, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'task_cards.html', {'page_obj': page_obj})
