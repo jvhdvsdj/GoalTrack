9be00952d297840e1d6f7f3b915afd1c187eeccd
jvhdvsdj <romkavasyliuk@gmail.com>
2026-05-23 07:38:45 +0300

    Add Habit model to Tasks app

--- django_project/Tasks/models.py
+++ django_project/Tasks/models.py
@@ -4,3 +4,7 @@ class Task(models.Model):
     title = models.CharField(max_length = 200)
     description = models.TextField()
     deadline = models.DateTimeField(null=True, blank=True, verbose_name="Термін виконання", help_text="Оберіть дату та час для зворотного відліку")
+
+class Habit(models.Model):
+    name = models.CharField(max_length = 100)
+    last_reset = models.DateField(auto_now=True)
