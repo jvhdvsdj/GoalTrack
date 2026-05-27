f33b3fdbdcc18eaf071d00e2f2d4d550b983ef30
jvhdvsdj <romkavasyliuk@gmail.com>
2026-05-27 15:59:29 +0300

    Update admin.py

--- django_project/Tasks/admin.py
+++ django_project/Tasks/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Task, Habit
+from .models import Task, Habit, HabitCheck
+
 
 admin.site.register(Task)
 admin.site.register(Habit)
+admin.site.register(HabitCheck)
 
 
